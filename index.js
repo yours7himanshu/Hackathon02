afd175527d38d173c50ba06ba79ec0555c94e406
Himanshu <dinkarhimanshu78@gmail.com>
2023-09-07 17:17:12 +0530

    initial commit

--- index.js
+++ index.js
@@ -62,3 +62,13 @@ function changeFestival() {
 setInterval(changeMonuments, 2000); // Change images for "monuments" class every 2 seconds
 setInterval(changeFood, 1000);      // Change images for "food" class every 1.5 seconds
 setInterval(changeFestival, 2000);  // Change images for "festival" class every 3 seconds
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
